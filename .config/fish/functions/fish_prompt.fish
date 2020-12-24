9245ddcc8ecd13218fb439d962b5738a6d6d414e
blissful <blissful@sunsetglow.net>
2020-12-24 18:18:07 -0500

    update fish cfg

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -6,7 +6,7 @@ function fish_prompt --description 'Write out the prompt'
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
     set -l prefix
-    set -l suffix '$'
+    set -l suffix '>'
 
     if contains -- $USER root toor
         if set -q fish_color_cwd_root
@@ -24,5 +24,5 @@ function fish_prompt --description 'Write out the prompt'
     # Write pipestatus
     set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
 
-    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (set_color $fish_color_vcs) (fish_vcs_prompt) $normal $prompt_status " " (set_color $fish_color_suffix) $suffix $normal " "
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (prompt_vcs) $prompt_status " " (set_color $fish_color_suffix) $suffix $normal " "
 end
