57996d43b2bc315d935496413771e514381b0e00
blissful <blissful@sunsetglow.net>
2020-12-30 21:06:57 -0500

    update

--- .config/fish/conf.d/setup.fish
+++ .config/fish/conf.d/setup.fish
@@ -1,27 +1,27 @@
-set GTK_IM_MODULE ibus
-set XMODIFIERS @im-bus
-set QT_IM_MODULE ibus
+set -g GTK_IM_MODULE ibus
+set -g XMODIFIERS @im-bus
+set -g QT_IM_MODULE ibus
 ibus-daemon -drx
 
 # opam configuration
 # test -r /home/azul/.opam/opam-init/init.sh && . /home/azul/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
 
-set npm_config_prefix $HOME/.node_modules
-set GEM_HOME $HOME/.gems
-set TEXMFHOME $HOME/.texmf
+set -g npm_config_prefix $HOME/.node_modules
+set -g GEM_HOME $HOME/.gems
+set -g TEXMFHOME $HOME/.texmf
 source $HOME/.cargo/env
 
-set PATH "$PATH:$HOME/.poetry/bin"
-set PATH "$PATH:$HOME/.local/bin"
-set PATH "$PATH:$HOME/.node_modules/bin"
-set PATH "$PATH:$HOME/.cargo/bin"
-set PATH "$PATH:$HOME/.gems/bin"
-set PATH "$PATH:$HOME/.pyenv/bin"
-set PATH "$PATH:$HOME/.cabal/bin"
-set PATH "$PATH:$HOME/.gem/ruby/2.7.0/bin"
-set PATH "$PATH:$HOME/.yarn/bin"
-set PATH "$PATH:$HOME/.config/yarn/global/node_modules/.bin"
-set PATH "$PATH:$HOME/.git-fuzzy/bin"
+set -g PATH "$PATH:$HOME/.poetry/bin"
+set -g PATH "$PATH:$HOME/.local/bin"
+set -g PATH "$PATH:$HOME/.node_modules/bin"
+set -g PATH "$PATH:$HOME/.cargo/bin"
+set -g PATH "$PATH:$HOME/.gems/bin"
+set -g PATH "$PATH:$HOME/.pyenv/bin"
+set -g PATH "$PATH:$HOME/.cabal/bin"
+set -g PATH "$PATH:$HOME/.gem/ruby/2.7.0/bin"
+set -g PATH "$PATH:$HOME/.yarn/bin"
+set -g PATH "$PATH:$HOME/.config/yarn/global/node_modules/.bin"
+set -g PATH "$PATH:$HOME/.git-fuzzy/bin"
 
 [ -f "/home/azul/.ghcup/env" ] && source "/home/azul/.ghcup/env" # ghcup-env
 
