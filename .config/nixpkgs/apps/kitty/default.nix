1ac75837bf1615ca200ef1fe34beba1d53ae8297
blissful <blissful@sunsetglow.net>
2021-04-08 11:36:45 -0400

    fix kitty switch

---
+++ .config/nixpkgs/apps/kitty/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+let
+  # TODO: Inject the GL wrapper per-computer.
+  nixGLNvidia = (
+    pkgs.callPackage "${builtins.fetchTarball {
+      url = https://github.com/guibou/nixGL/archive/17c1ec63b969472555514533569004e5f31a921f.tar.gz;
+      sha256 = "0yh8zq746djazjvlspgyy1hvppaynbqrdqpgk447iygkpkp3f5qr";
+    }}/nixGL.nix" {}
+  ).nixGLNvidia;
+  wrappedKitty = pkgs.writeScriptBin "kitty" ''
+    #!${pkgs.stdenv.shell}
+    ${nixGLNvidia}/bin/nixGLNvidia ${pkgs.kitty}/bin/kitty "$@"
+  '';
+  wrappedPkg = pkgs.symlinkJoin {
+    name = "kitty";
+    paths = [ wrappedKitty pkgs.kitty ];
+  };
+in
+{
+  home.packages = [ wrappedPkg ];
+}
