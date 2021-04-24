fb261b9f07c785397fa25dcf7ae9caa7ffd16729
blissful <blissful@sunsetglow.net>
2021-04-24 15:08:23 -0400

    fix yy launcher; calibre viewer tweaks

--- nixpkgs/scripts/i3-yy.sh
+++ nixpkgs/scripts/i3-yy.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-text_files="$(find "${HOME}/texts" -type f \( -name '*.pdf' -o -name '*.html' \) -printf '%P\n' | sed 's/^/texts\//')"
-book_files="$(find "${HOME}/books" -type f \( -name '*.pdf' -o -name '*.epub' -o -name '*.azw3' \) -printf '%P\n' | sed 's/^/books\//')"
+text_files="$(find "$HOME/texts" -type f \( -name '*.pdf' -o -name '*.html' \) -printf '%P\n' | sed 's/^/texts\//')"
+book_files="$(find "$HOME/books" -type f \( -name '*.pdf' -o -name '*.epub' -o -name '*.azw3' \) -printf '%P\n' | sed 's/^/books\//')"
 
-files="${text_files}\n${book_files}"
-selected=$(printf "%s" "$files" | fzf --layout=reverse)
+files="$text_files\n$book_files"
+selected=$(printf '%s' "$files" | fzf --layout=reverse)
 
 if [[ -z "$selected" ]]; then
 	exit 1
 fi
 
-filepath="${HOME}/${selected}"
+filepath=$(printf '%q' "$HOME/$selected")
 
 if [[ "$filepath" == *.pdf ]]; then
-	i3-msg -t command exec "\"evince '${filepath}'\""
+	i3-msg -t command exec \""evince $filepath"\"
 elif [[ "$filepath" == *.html ]]; then
-	i3-msg -t command exec "\"firefox '${filepath}'\""
+	i3-msg -t command exec \""firefox $filepath"\"
 elif [[ "$filepath" == *.epub ]] || [[ "$filepath" == *.azw3 ]]; then
-	i3-msg -t command exec "\"ebook-viewer '${filepath}'\""
+	i3-msg -t command exec \""ebook-viewer $filepath"\"
 fi
