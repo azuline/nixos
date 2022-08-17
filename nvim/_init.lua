a8adc187b2542d5b11f450517667724cb3bac677
blissful <blissful@sunsetglow.net>
2022-08-17 11:21:30 -0400

    restructure init.lua

--- nvim/_init.lua
+++ nvim/_init.lua
@@ -23,6 +23,31 @@ do -- Load the plugins.
   -- General dependency.
   Plug("nvim-lua/plenary.nvim")
 
+  do -- Editor augmentation
+    -- Text objects for expanding/contracting syntactic constructs
+    Plug("RRethy/nvim-treesitter-textsubjects")
+    -- Bullet points, because the replacement Markdown plugins all suck
+    Plug("dkarter/bullets.vim")
+    -- Extra text objects for braces/quotes/tags. targets.vim gets us things
+    -- _inside_ the tags, vim-surround has fancy stuff for _outside_ the tags.
+    Plug("wellle/targets.vim")
+    Plug("tpope/vim-surround")
+    -- Comment/uncomment assistance (because I'm slow)
+    Plug("tpope/vim-commentary")
+    -- Pluralization & case coercion
+    Plug("tpope/vim-abolish")
+    -- Give me readline in command mode!
+    Plug("tpope/vim-rsi")
+    -- Repeat for plugins
+    Plug("tpope/vim-repeat")
+    -- Text alignment operator
+    Plug("tommcdo/vim-lion")
+    -- Toggle between singleline and multiline syntax
+    Plug("AndrewRadev/splitjoin.vim")
+    -- Sublime style multiple cursors
+    Plug("mg979/vim-visual-multi")
+  end
+
   do -- Navigation
     -- Fuzzy finder
     Plug("junegunn/fzf", { ["do"] = vim.fn["fzf#install()"] })
@@ -31,7 +56,7 @@ do -- Load the plugins.
     Plug("ms-jpq/chadtree", { branch = "chad", ["do"] = "python3 -m chadtree deps" })
   end
 
-  do -- Visuals
+  do -- Window visuals
     -- Theme
     Plug("drewtempelmeyer/palenight.vim")
     -- Statusline
@@ -58,31 +83,6 @@ do -- Load the plugins.
     Plug("KeitaNakamura/tex-conceal.vim", { ["for"] = "tex" })
     -- CSS
     Plug("ap/vim-css-color")
-  end
-
-  do -- Editor augmentation
-    -- Text objects for expanding/contracting syntactic constructs
-    Plug("RRethy/nvim-treesitter-textsubjects")
-    -- Bullet points, because the replacement Markdown plugins all suck
-    Plug("dkarter/bullets.vim")
-    -- Extra text objects for braces/quotes/tags. targets.vim gets us things
-    -- _inside_ the tags, vim-surround has fancy stuff for _outside_ the tags.
-    Plug("wellle/targets.vim")
-    Plug("tpope/vim-surround")
-    -- Comment/uncomment assistance (because I'm slow)
-    Plug("tpope/vim-commentary")
-    -- Pluralization & case coercion
-    Plug("tpope/vim-abolish")
-    -- Give me readline in command mode!
-    Plug("tpope/vim-rsi")
-    -- Repeat for plugins
-    Plug("tpope/vim-repeat")
-    -- Text alignment operator
-    Plug("tommcdo/vim-lion")
-    -- Toggle between singleline and multiline syntax
-    Plug("AndrewRadev/splitjoin.vim")
-    -- Sublime style multiple cursors
-    Plug("mg979/vim-visual-multi")
   end
 
   do -- Development Tooling
