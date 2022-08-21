81af0262264af9cb98ba215d1ecff664d74adac6
blissful <blissful@sunsetglow.net>
2022-08-21 08:40:12 -0400

    fix hash

--- nixpkgs/pkgs/fish/default.nix
+++ nixpkgs/pkgs/fish/default.nix
@@ -22,7 +22,7 @@
           owner = "lilyball";
           repo = "nix-env.fish";
           rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
-          sha256 = "sha256-tUSGO7qF12AxgljduQDIVbBxrMpzIoHeKpi1dxNSTLI=";
+          sha256 = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
         };
       }
     ];
