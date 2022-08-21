eeddb99e86edc957ca250dbf3fe65d6a624d4ba8
blissful <blissful@sunsetglow.net>
2022-08-21 09:24:39 -0400

    multi-host polybar

--- nixpkgs/polybar/default.nix
+++ nixpkgs/polybar/default.nix
@@ -1,13 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, specialArgs, ... }:
 
 {
   services.polybar = {
     enable = true;
     package = pkgs.polybarFull;
     config = ./config.ini;
-    script = ''
-      polybar left &
-      polybar right &
-    '';
+    script = (
+      if specialArgs.host == "splendor" then
+        ''
+          polybar left &
+          polybar right &
+        ''
+      else if specialArgs.host == "neptune" then
+        ''
+          polybar neptune &
+        ''
+      else throw "Invalid host for polybar."
+    );
   };
 }
