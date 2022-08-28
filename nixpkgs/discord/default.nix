32693f74010b5c4c489491ef15fb098385a39995
blissful <blissful@sunsetglow.net>
2022-08-28 16:32:09 -0400

    simplify discord derivation

--- nixpkgs/discord/default.nix
+++ nixpkgs/discord/default.nix
@@ -1,16 +1,9 @@
 { pkgs, specialArgs, ... }:
 
 let
-  # discord = pkgs.discord.overrideAttrs (old: rec {
-  #   src = specialArgs.src.discord;
-  # });
-  discord = pkgs.callPackage ./base.nix rec {
-    pname = "discord";
-    binaryName = "Discord";
-    desktopName = "Discord";
-    version = "0.0.19";
+  discord = pkgs.discord.overrideAttrs (_: rec {
     src = specialArgs.src.discord;
-  };
+  });
 in
 {
   home.packages = [ discord ];
