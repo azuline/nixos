6c320136fbbdfddf3fd253dc7ae8a51a0d496096
blissful <blissful@sunsetglow.net>
2022-09-11 03:06:14 -0400

    tray? :plead:

--- nix/home/polybar/default.nix
+++ nix/home/polybar/default.nix
@@ -23,7 +23,8 @@
   systemd.user.targets.tray = {
     Unit = {
       Description = "Home Manager System Tray";
-      Requires = [ "graphical-session-pre.target" ];
+      Requires = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
     };
   };
 }
