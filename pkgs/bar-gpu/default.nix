39576ebd346fed695af5a672010ea92ffd84f30e
blissful <blissful@sunsetglow.net>
2023-09-16 09:35:07 -0400

    more bar fixes

--- pkgs/bar-gpu/default.nix
+++ pkgs/bar-gpu/default.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "bar-gpu" ''
-  /usr/bin/nvidia-smi \
+  /run/current-system/sw/bin/nvidia-smi \
     --query-gpu=utilization.gpu \
     --format=csv,noheader,nounits \
     | ${pkgs.gawk}/bin/awk '{ printf "0.%02i", $1}'
