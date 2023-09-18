f98d1ecbc311ca6573b35b94e80fe363d6a47a31
blissful <blissful@sunsetglow.net>
2023-09-18 00:35:23 -0400

    move cursor to x11

--- home/theme/default.nix
+++ home/theme/default.nix
@@ -15,12 +15,17 @@
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
-    cursorTheme = {
-      name = "Quintom_Ink";
-      package = pkgs.quintom-cursor-theme;
-      size = 12;
-    };
   };
+
+  home.pointerCursor = {
+    size = 56;
+    name = "Nordzy-cursors";
+    package = pkgs.nordzy-cursor-theme;
+    x11.enable = true;
+  };
+  # While X is managed by NixOS, we enable this here so that the pointerCursor
+  # `xsetroot` command gets written.
+  xsession.enable = true;
 
   # We should set Qt up here, but it didn't really work the way we desired. So
   # instead, we set it at the NixOS level.
