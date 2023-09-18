80bc401eb5bc71705c96f05712c5f209db1fdc2a
blissful <blissful@sunsetglow.net>
2023-09-18 11:40:51 -0400

    update i3-screenshot to create dir if missing and err if no arg

--- pkgs/i3-screenshot/default.nix
+++ pkgs/i3-screenshot/default.nix
@@ -1,16 +1,21 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "i3-screenshot" ''
-  # Use `-` instead of `:` because syncthing can't sync colons to Android.
-  filename=~/images/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
-  case "$1" in
+  ${pkgs.coreutils}/bin/mkdir -p "$HOME/images/Screenshots"
 
+  # Use `-` instead of `:` because syncthing can't sync colons to Android.
+  filename="$HOME/images/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png"
+
+  case "$1" in
   select)
     ${pkgs.maim}/bin/maim --select "$filename"
     ;;
   screen)
     ${pkgs.maim}/bin/maim "$filename"
     ;;
+  *)
+    echo "Argument required: select, screen"
+    exit 1
   esac
 
   ${pkgs.xclip}/bin/xclip -sel c -t image/png -i "$filename"
