72040c603433453a38e491f5e7cf6ce003e76b26
blissful <blissful@sunsetglow.net>
2023-09-18 00:11:17 -0400

    rm xsessionrc

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -2,7 +2,6 @@
 
 let
   configBase = builtins.readFile ./config;
-  xsessionrcBase = builtins.readFile ./xsessionrc;
   config = (
     if specialArgs.sys.host == "splendor" then configBase + ''
       gaps inner 16
@@ -12,15 +11,7 @@ let
       gaps outer 2
     '' else throw "Invalid host in i3."
   );
-  xsessionrc = (
-    if specialArgs.sys.host == "splendor" then xsessionrcBase + ''
-      xrandr --output HDMI-0 --right-of DP-0
-      xrandr --output DP-0 --primary
-    '' else if specialArgs.sys.host == "haiqin" then xsessionrcBase
-    else throw "Invalid host in i3."
-  );
 in
 {
   xdg.configFile."i3/config".text = config;
-  home.file.".xsessionrc".text = xsessionrc;
 }
