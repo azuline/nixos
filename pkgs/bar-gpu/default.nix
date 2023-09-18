79f935d8e07cfa793efee22bdf0e40363b2b5fe3
blissful <blissful@sunsetglow.net>
2023-09-18 12:19:52 -0400

    fix gpu bar having two values

--- pkgs/bar-gpu/default.nix
+++ pkgs/bar-gpu/default.nix
@@ -1,7 +1,9 @@
 { pkgs }:
 
+# Currently hardcodes the ID of the GPU bus (A) we use on splendor.
 pkgs.writeShellScriptBin "bar-gpu" ''
   /run/current-system/sw/bin/nvidia-smi \
+    --id=00000000:0A:00.0 \
     --query-gpu=utilization.gpu \
     --format=csv,noheader,nounits \
     | ${pkgs.gawk}/bin/awk '{ printf "0.%02i", $1}'
