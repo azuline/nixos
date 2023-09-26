271cfb058a9a47cf84da4c2e7130c6760fddcaeb
blissful <blissful@sunsetglow.net>
2023-09-26 01:20:40 -0400

    clone haiqin to neptune for 2nd laptop config

---
+++ systems/neptune/configuration.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, ... }:
+
+{
+  system.stateVersion = "22.11";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.max-jobs = 8;
+  nixpkgs.config.allowUnfree = true;
+  # Doesn't work in Flakes.
+  programs.command-not-found.enable = false;
+
+  imports = [
+    ./hardware-configuration.nix
+    ./desktop.nix
+  ];
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    initrd.luks.devices.root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    };
+    # True by default; creates a warning when other parameters are unset. So we
+    # disable it. See https://github.com/NixOS/nixpkgs/issues/254807.
+    swraid.enable = false;
+  };
+
+  networking = {
+    hostName = "haiqin";
+    networkmanager.enable = true;
+    extraHosts = ''
+    '';
+    firewall = {
+      allowedTCPPorts = [
+        22000 # syncthing
+      ];
+      # For tailscale https://github.com/tailscale/tailscale/issues/4432.
+      checkReversePath = "loose";
+      interfaces.tailscale0 = {
+        allowedTCPPorts = [
+          22 # ssh
+        ];
+      };
+    };
+  };
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "ter-i32b";
+    packages = with pkgs; [ terminus_font ];
+    # Load the larger console font earlier in the boot process.
+    earlySetup = true;
+    useXkbConfig = true;
+  };
+
+  users.users.blissful = {
+    createHome = true;
+    home = "/home/blissful";
+    uid = 1000;
+    shell = pkgs.fish;
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "audio" "disk" "networkmanager" "docker" ];
+  };
+
+  environment = {
+    interactiveShellInit = builtins.readFile ./bashrc;
+    # Unset NixOS default shell aliases.
+    shellAliases = {
+      l = null;
+      ls = null;
+      ll = null;
+    };
+    variables = {
+      EDITOR = "nvim";
+      XCURSOR_SIZE = "64";
+    };
+    systemPackages = with pkgs; [
+      curl
+      jq
+      mesa
+      neovim
+      intel-gpu-tools
+      glxinfo
+      networkmanagerapplet
+      powertop
+      vim
+      wget
+      nftables
+      wireguard-tools
+    ];
+  };
+  virtualisation.docker.enable = true;
+  services.tailscale.enable = true;
+  programs.fish.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+      START_CHARGE_THRESH_BAT0 = 90;
+      STOP_CHARGE_THRESH_BAT0 = 97;
+      RUNTIME_PM_ON_BAT = "auto";
+    };
+  };
+}
