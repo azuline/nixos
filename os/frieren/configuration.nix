e303852207f473319821f2894cc58afc8ea0d226
blissful <blissful@sunsetglow.net>
2023-12-15 10:43:18 -0500

    frieren

---
+++ os/frieren/configuration.nix
@@ -0,0 +1,180 @@
+{ config, lib, pkgs, pkgs-stable, ... }:
+
+let
+  mdadmConf = ''
+    HOMEHOST frieren
+    ARRAY /dev/md/0 level=raid1 num-devices=2 metadata=1.2 name=frieren:0 UUID=dc490347-1616-47e6-a19e-3b7b669c48fb
+       devices=/dev/nvme0n1p2,/dev/nvme1n1p1
+    ARRAY /dev/md/boot level=raid1 num-devices=2 metadata=1.0 name=frieren:boot UUID=C54C-D58B
+       devices=/dev/nvme0n1p1,/dev/nvme1n1p1
+  '';
+in
+{
+  system.stateVersion = "21.11";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.max-jobs = 8;
+  nixpkgs.config.allowUnfree = true;
+  # Doesn't work in Flakes.
+  programs.command-not-found.enable = false;
+
+  imports = [
+    ./hardware-configuration.nix
+    ./services.nix
+  ];
+
+  time.timeZone = "UTC";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  networking = {
+    hostName = "frieren";
+    useDHCP = false;
+    interfaces.eno1.ipv4.addresses = [{ address = "147.135.10.32"; prefixLength = 24; }];
+    defaultGateway = "147.135.10.254";
+    nameservers = [ "1.1.1.1" ];
+    firewall = {
+      allowedTCPPorts = [
+        22 # ssh
+        2222 # boot ssh
+        80 # http
+        443 # https
+      ];
+      # For tailscale https://github.com/tailscale/tailscale/issues/4432.
+      checkReversePath = "loose";
+      interfaces.tailscale0 = {
+        allowedTCPPorts = [
+          22 # ssh
+          4646 # nomad
+          8500 # consul
+          9001 # thelounge
+        ];
+      };
+    };
+  };
+
+  boot = {
+    loader = {
+      systemd-boot.enable = false;
+      efi.canTouchEfiVariables = false;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+        enableCryptodisk = true;
+        device = "nodev";
+      };
+      supportsInitrdSecrets = true;
+    };
+    kernelParams = [ "ip=147.135.10.32::147.135.10.254:255.255.255.0:frieren:eno1:off:1.1.1.1:" ];
+    initrd = {
+      kernelModules = [ "cryptd" "aesni_intel" "igb" ];
+      network = {
+        enable = true;
+        ssh = {
+          enable = true;
+          port = 2222; # ssh port during boot for luks decryption; it will have a different host key from post-boot ssh
+          authorizedKeys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK7+XlAgpi6eSC0GjgUq1bMOtGOzrOODBTkID8LuuZAL splendor"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPT/WSwL6oN2JNVy1juCAKk/gJ7KFgPVko3BX0nO3neQ haiqin"
+          ];
+          hostKeys = [ "/etc/ssh/initrd_ssh_host_ed25519_key" ];
+        };
+        # Set the shell profile to meet SSH connections with a decryption
+        # prompt that writes to /tmp/continue if successful.
+        # https://mth.st/blog/nixos-initrd-ssh/
+        postCommands = ''
+          echo "mdadm --assemble /dev/md/0 && mdadm --assemble /dev/md/boot && cryptsetup open /dev/md/0 enc-pv && echo 'done' > /tmp/continue" >> /root/.profile
+          echo "starting sshd..."
+        '';
+      };
+      # Block the boot process until /tmp/continue is written to
+      postDeviceCommands = ''
+        echo 'waiting for root device to be opened...'
+        mkfifo /tmp/continue
+        cat /tmp/continue
+      '';
+      luks = {
+        forceLuksSupportInInitrd = true;
+      };
+      secrets = {
+        "/etc/ssh/initrd_ssh_host_ed25519_key" = "/etc/ssh/initrd_ssh_host_ed25519_key";
+      };
+    };
+  };
+
+  # Better for the SSD.
+  fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
+  # Make the mdadm conf available in stage2.
+  environment.etc."mdadm.conf".text = mdadmConf;
+  # The RAIDs are assembled in stage1, so we need to make the config available there too.
+  boot.swraid.mdadmConf = mdadmConf;
+
+  environment = {
+    interactiveShellInit = builtins.readFile ./bashrc;
+    # Unset NixOS default shell aliases.
+    shellAliases = {
+      l = null;
+      ls = null;
+      ll = null;
+    };
+    variables = {
+      EDITOR = "nvim";
+      LC_COLLATE = "C"; # Lets special characters sort last; doesn't ignore them.
+    };
+    systemPackages = with pkgs; [
+      certbot
+      curl
+      git
+      jq
+      neovim
+      pinentry-curses
+      powertop
+      presage
+      smartmontools
+      vim
+      wget
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  users = {
+    users = {
+      cron = {
+        isSystemUser = true;
+        uid = 1001;
+        group = "cron";
+      };
+      blissful = {
+        createHome = true;
+        home = "/home/blissful";
+        uid = 1000;
+        shell = pkgs.fish;
+        isNormalUser = true;
+        extraGroups = [ "wheel" "docker" "nomad" "otel" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK7+XlAgpi6eSC0GjgUq1bMOtGOzrOODBTkID8LuuZAL splendor"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPT/WSwL6oN2JNVy1juCAKk/gJ7KFgPVko3BX0nO3neQ haiqin"
+        ];
+      };
+    };
+    groups = {
+      cron = {
+        gid = 1001;
+      };
+      presage = {
+        gid = 2001;
+        members = [ "blissful" "cron" "root" ];
+      };
+    };
+  };
+
+  virtualisation.docker.enable = true;
+  services.tailscale.enable = true;
+  programs.fish.enable = true;
+}
---
+++ os/frieren/services.nix
@@ -0,0 +1,127 @@
+{ pkgs-stable, ... }:
+
+let
+  nomadConfig = "/etc/nixos/os/frieren/nomad";
+  consulConfig = "/etc/nixos/os/frieren/consul";
+in
+{
+  environment.systemPackages = with pkgs-stable; [
+    nomad
+    consul
+    envoy
+    opentelemetry-collector-contrib
+  ];
+
+  # Install CNI plugins into /opt/cni.
+  system.activationScripts.cni.text = ''
+    mkdir -p /opt
+    rm -f /opt/cni
+    ln -sf ${pkgs-stable.cni-plugins} /opt/cni
+  '';
+
+  environment.variables = {
+    NOMAD_ADDR = "http://100.71.28.44:4646";
+    CONSUL_HTTP_ADDR = "http://100.71.28.44:8500";
+  };
+
+  users = {
+    users = {
+      consul = {
+        isSystemUser = true;
+        group = "consul";
+        uid = 1002;
+      };
+      otel = {
+        isSystemUser = true;
+        group = "otel";
+        uid = 1003;
+      };
+    };
+    groups = {
+      consul.gid = 1002;
+      otel.gid = 1003;
+    };
+  };
+
+  # Need to configure network namespace for Consul.
+  # https://developer.hashicorp.com/nomad/docs/integrations/consul-connect#cni-plugins
+  boot.kernel.sysctl = {
+    "net.bridge.bridge-nf-call-arptables" = 1;
+    "net.bridge.bridge-nf-call-ip6tables" = 1;
+    "net.bridge.bridge-nf-call-iptables" = 1;
+  };
+
+  systemd.services.nomad = {
+    path = with pkgs-stable; [ nomad consul iproute iptables cni-plugins ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "root";
+      Group = "root";
+      ExecReload = "/bin/kill -HUP $MAINPID";
+      ExecStart = "@${pkgs-stable.nomad}/bin/nomad nomad agent -config ${nomadConfig}";
+      KillMode = "process";
+      KillSignal = "SIGINT";
+      LimitNOFILE = 65536;
+      LimitNPROC = "infinity";
+      Restart = "on-failure";
+      RestartSec = 2;
+      TasksMax = "infinity";
+      OOMScoreAdjust = -1000;
+    };
+  };
+
+  systemd.services.consul = {
+    path = with pkgs-stable; [ consul envoy iproute iptables cni-plugins ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "consul";
+      Group = "consul";
+      ExecReload = "/bin/kill -HUP $MAINPID";
+      ExecStart = "@${pkgs-stable.consul}/bin/consul consul agent -config-dir=${consulConfig}";
+      KillMode = "process";
+      KillSignal = "SIGTERM";
+      LimitNOFILE = 65536;
+      Restart = "on-failure";
+      RestartSec = 2;
+      OOMScoreAdjust = -1000;
+    };
+  };
+
+  systemd.services.otelcollector = {
+    path = with pkgs-stable; [ opentelemetry-collector-contrib ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "otel";
+      Group = "otel";
+      ExecReload = "/bin/kill -HUP $MAINPID";
+      ExecStart = "${pkgs-stable.opentelemetry-collector-contrib}/bin/otelcontribcol --config=file:/data/otel/config.yaml";
+      KillMode = "process";
+      KillSignal = "SIGTERM";
+      LimitNOFILE = 65536;
+      Restart = "on-failure";
+      RestartSec = 2;
+      OOMScoreAdjust = -1000;
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "blissful@sunsetglow.net";
+    certs."sunsetglow.net" = {
+      domain = "sunsetglow.net";
+      extraDomainNames = [ "*.sunsetglow.net" ];
+      dnsProvider = "porkbun";
+      environmentFile = "/secrets/acme/credentials";
+      postRun = ''
+        source /secrets/acme/nomad.env
+        ${pkgs-stable.nomad}/bin/nomad job allocs -json nginx | ${pkgs-stable.jq}/bin/jq -r '.[0].ID' | ${pkgs-stable.findutils}/bin/xargs ${pkgs-stable.nomad}/bin/nomad alloc restart
+      '';
+    };
+  };
+}
