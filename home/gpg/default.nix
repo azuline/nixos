20ff9eba2b8c3190cc3f1830a0e5a56150fc5655
blissful <blissful@sunsetglow.net>
2023-12-25 11:01:32 -0500

    fix home-manager.services.gpg-agent after bumping

--- home/gpg/default.nix
+++ home/gpg/default.nix
@@ -3,6 +3,7 @@
 {
   programs.gpg.enable = true;
   services.gpg-agent.enable = true;
+  services.gpg-agent.pinentryFlavor = null;
   services.gpg-agent.extraConfig = ''
     pinentry-program ${pkgs.pinentry}/bin/pinentry
     default-cache-ttl 14400
