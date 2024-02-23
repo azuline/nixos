9c5b742a58f788a0cad8067fbd8bd7b12e187ee7
blissful <blissful@sunsetglow.net>
2024-02-23 14:19:22 -0500

    fix cdrama rss timer

--- home/cdrama-rss/default.nix
+++ home/cdrama-rss/default.nix
@@ -4,15 +4,13 @@
   systemd.user.services."cdrama-rss" = {
     Unit.Description = "cdrama rss trigger";
     Service.ExecStart = "${pkgs.bash}/bin/bash /home/blissful/scripts/cdrama/run.sh";
+    Service.Type = "oneshot";
     Install.WantedBy = [ "default.target" ];
-    serviceConfig.Type = "oneshot";
   };
   systemd.user.timers."cdrama-rss" = {
     Unit.Description = "cdrama rss trigger";
-    Timer = {
-      OnCalendar = "hourly";
-      Persistent = true;
-    };
+    Timer.OnCalendar = "hourly";
+    Timer.Persistent = true;
     Install.WantedBy = [ "timers.target" ];
   };
 }
