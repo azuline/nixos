b4f54a344b36e4af3fc19e091cc88a27ad1145ee
blissful <blissful@sunsetglow.net>
2024-02-22 22:44:13 -0500

    fix music bar script to show hours

--- pkgs/bar-now-playing/default.nix
+++ pkgs/bar-now-playing/default.nix
@@ -8,14 +8,14 @@ writeShellScriptBin "bar-now-playing" ''
   # Redirect stderr to /dev/null
   exec 2>/dev/null
 
-  # Print N/A if there is an error (which occurs if mpv is not running).
-  trap 'exit_handler' EXIT
   exit_handler() {
     if [ $? -ne 0 ]; then
       echo "N/A"
       exit 0
     fi
   }
+  # Print N/A if there is an error (which occurs if mpv is not running).
+  trap 'exit_handler' EXIT
 
   get_property() {
     echo "{ \"command\": [\"get_property_string\", \"$1\"] }" | socat - "$XDG_RUNTIME_DIR/mpv-playmusic" | jq -r ".data"
@@ -54,10 +54,10 @@ writeShellScriptBin "bar-now-playing" ''
       artists="$artists (feat. $guest_artists)"
     fi
 
-    time_pos="$(get_property time-pos)"
-    time_total="$(echo "$(get_property time-remaining) + $time_pos" | bc)"
-    time_pos="$(date -d "@$time_pos" +"%-M:%S")"
-    time_total="$(date -d "@$time_total" +"%-M:%S")"
+    time_pos_sec="$(get_property time-pos)"
+    time_pos="$(date -u -d "@$time_pos_sec" +"%H:%M:%S" | sed 's/^00://' | sed 's/^0//')"
+    time_total_sec="$(echo "$(get_property time-remaining) + $time_pos_sec" | bc)"
+    time_total="$(date -u -d "@$time_total_sec" +"%H:%M:%S" | sed 's/^00://' | sed 's/^0//')"
 
     playlist_pos="$(get_property playlist-pos | tr -d "\n" | cat - <(echo "+1") | bc)"
     playlist_total="$(get_property playlist-count)"
