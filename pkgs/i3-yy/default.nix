fb118d0748ec2abb4c69fa14b1c71bf54bff7a78
blissful <blissful@sunsetglow.net>
2024-03-14 20:17:25 -0400

    ok

--- pkgs/i3-yy/default.nix
+++ pkgs/i3-yy/default.nix
@@ -3,7 +3,7 @@
 writeShellScriptBin "i3-yy" ''
   book_files="$(find "$HOME/books" -type f \( -name '*.pdf' -o -name '*.epub' -o -name '*.azw3' \) -printf '%P\n' | sed 's/^/books\//')"
   zotero_files="$(find "$HOME/Zotero/storage" -type f \( -name '*.pdf' -o -name '*.epub' -o -name '*.azw3' -o -name '*.html' \) -printf '%P\n' | sed 's/^/Zotero\/storage\//')"
-  atelier_files="$(find "$HOME/atelier/_store" -type f \( -name '*.pdf' -o -name '*.epub' -o -name '*.azw3' -o -name '*.html' \) -printf '%P\n' | sed 's/^/atelier\/store\//')"
+  atelier_files="$(find "$HOME/atelier/_store" -type f \( -name '*.pdf' -o -name '*.epub' -o -name '*.azw3' -o -name '*.html' \) -printf '%P\n' | sed 's/^/atelier\/_store\//')"
 
   files="$zotero_files\n$book_files\n$atelier_files"
   selected=$(printf '%s' "$files" | fzf --layout=reverse --with-nth 1.. --delimiter '/')
