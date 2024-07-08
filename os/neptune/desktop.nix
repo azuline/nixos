d7da702c83a8ff797ae43c0d9e4a181bf5cf5f47
blissful <blissful@sunsetglow.net>
2024-07-08 10:29:29 -0400

    fix i3 touchpad/nipple stuff on laptops

--- os/neptune/desktop.nix
+++ os/neptune/desktop.nix
@@ -18,9 +18,14 @@
         layout = "us";
         options = "altwin:swap_lalt_lwin,caps:escape_shifted_capslock";
       };
-      # Disable libinput because I don't want to use the touchpad.
-      libinput.enable = false;
+      displayManager.sessionCommands = ''
+        ${pkgs.xorg.xinput}/bin/xinput --set-prop 'TPPS/2 Elan TrackPoint' 'Evdev Wheel Emulation' 1
+        ${pkgs.xorg.xinput}/bin/xinput --set-prop 'TPPS/2 Elan TrackPoint' 'Evdev Wheel Emulation Button' 2
+        ${pkgs.xorg.xinput}/bin/xinput --set-prop 'TPPS/2 Elan TrackPoint' 'Evdev Wheel Emulation Axes' 6 7 4 5
+      '';
     };
+    # Disable libinput because I don't want to use the touchpad.
+    libinput.enable = false;
     displayManager = {
       defaultSession = "none+i3";
       # It's fine to enable autologin since we have disk encryption.
@@ -28,11 +33,6 @@
         enable = true;
         user = "blissful";
       };
-      sessionCommands = ''
-        ${pkgs.xorg.xinput}/bin/xinput --set-prop 'TPPS/2 Elan TrackPoint' 'Evdev Wheel Emulation' 1
-        ${pkgs.xorg.xinput}/bin/xinput --set-prop 'TPPS/2 Elan TrackPoint' 'Evdev Wheel Emulation Button' 2
-        ${pkgs.xorg.xinput}/bin/xinput --set-prop 'TPPS/2 Elan TrackPoint' 'Evdev Wheel Emulation Axes' 6 7 4 5
-      '';
     };
   };
 
