476ab370b0f0b72455163a8c107897371824668e
blissful <blissful@sunsetglow.net>
2024-10-28 19:50:25 -0400

    nixify picom & switch to egl backend

--- home/picom/default.nix
+++ home/picom/default.nix
@@ -1,6 +1,20 @@
-{ specialArgs, ... }:
+{ ... }:
 
 {
-  services.picom.enable = true;
-  xdg.configFile."picom/picom.conf".source = ./picom.conf;
+  services.picom = {
+    enable = true;
+    backend = "egl";
+    fade = true;
+    fadeSteps = [ 0.05 0.05 ];
+    fadeDelta = 1;
+    vSync = true;
+    opacityRules = [
+      "95:class_g = 'Kitty' && !_NET_WM_STATE@:32a"
+      "0:_NET_WM_STATE@[0]:32a = '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[1]:32a = '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[2]:32a = '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[3]:32a = '_NET_WM_STATE_HIDDEN'"
+      "0:_NET_WM_STATE@[4]:32a = '_NET_WM_STATE_HIDDEN'"
+    ];
+  };
 }
