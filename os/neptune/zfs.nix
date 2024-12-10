211beb52a1aad848df3b2502c1ba3bb25d99b502
blissful <blissful@sunsetglow.net>
2024-12-10 09:33:52 -0800

    neptune more

--- os/neptune/zfs.nix
+++ os/neptune/zfs.nix
@@ -3,9 +3,16 @@
 {
   boot = {
     supportedFilesystems = [ "zfs" ];
-    zfs.forceImportRoot = false;
+    zfs = {
+      forceImportRoot = false;
+      # Testing this.
+      # requestEncryptionCredentials = [ "galatea" ];
+    };
     # Cap ARC at 4GB because I keep OOMing...
-    kernelParams = [ "zfs.zfs_arc_max=4294967296" ];
+    kernelParams = [
+      "zfs.zfs_arc_min=2147483648"
+      "zfs.zfs_arc_max=4294967296"
+    ];
   };
   # fileSystems."/galatea" = {
   #   device = "galatea";
