ca6bc3b82104a09c9b93e2f3c4892bed0d90812f
acid angel from asia <blissful@sunsetglow.net>
2025-05-13 16:10:48 -0700

    update deps

--- home/gpg/default.nix
+++ home/gpg/default.nix
@@ -3,7 +3,7 @@
 {
   programs.gpg.enable = true;
   services.gpg-agent.enable = true;
-  services.gpg-agent.pinentryPackage = null;
+  services.gpg-agent.pinentry.package = null;
   services.gpg-agent.extraConfig = ''
     pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
     default-cache-ttl 14400
