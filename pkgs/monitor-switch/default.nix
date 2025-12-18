fd2c3a9b6dde9d4e447f6879ceecb52952eeb904
acid angel from asia <blissful@sunsetglow.net>
2025-12-18 07:30:20 -0800

    monitor-switch for home

--- pkgs/monitor-switch/default.nix
+++ pkgs/monitor-switch/default.nix
@@ -8,8 +8,7 @@ writeShellScriptBin "monitor-switch" ''
 
   function monitorOn() {
     # Monitor on
-    xrandr --output DP-3 --auto --primary --above eDP-1
-    xrandr --output ${monitor} --auto --left-of DP-3
+    xrandr --output ${monitor} --auto --primary --above eDP-1
     sleep 1
     /home/blissful/backgrounds/apply.sh
     /home/blissful/backgrounds/gen_lock.sh
@@ -27,7 +26,6 @@ writeShellScriptBin "monitor-switch" ''
     # Monitor off
     xrandr --output eDP-1 --auto --primary
     xrandr --output ${monitor} --off
-    xrandr --output DP-3 --off
     sleep 1
     /home/blissful/backgrounds/apply.sh
     /home/blissful/backgrounds/gen_lock.sh
